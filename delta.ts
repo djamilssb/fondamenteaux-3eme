35a7ef42fd41e1305557fcc6927f4fa7948fb8ad
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-10-10 10:19:36 +0100

    c

---
+++ delta.ts
@@ -0,0 +1,10 @@
+function delta(a:number,b:number,c:number):number {
+    let d:number=0
+
+    d = b*b-4*a*c
+
+    return d
+
+}
+
+console.log(delta(4,5,3))
---
+++ root.ts
@@ -0,0 +1,24 @@
+var result:number[]=[0];
+function root(a:number,b:number,c:number):number[] {
+    let d:number = 0;
+    d = b*b-4*a*c;
+
+    if (d<0){
+        result[0] = 0
+        return result
+    }
+
+    if (d=0) {
+        result[1]=-b/(2*a)
+        return result
+    }
+
+    if (d>0) {
+        result[2]= -b - Math.sqrt(d) / 2*a
+        result[3]= -b + Math.sqrt(d) / 2*a
+        return result
+    }
+    return result
+}
+
+console.log(root(5,100,5))
