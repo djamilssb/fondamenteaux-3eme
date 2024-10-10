34c28788ca3c95115cc0c6417ca831e253a80432
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-10-10 10:27:08 +0100

    Update isPrime.ts

--- isPrime.ts
+++ isPrime.ts
@@ -2,11 +2,11 @@ function isPrime(n:number):boolean {
     let res : number  = 0
 
     for (let i=2;i<n;i++){
-        if(res%i != 0){
-            return true
+        if(res%i == 0){
+            return false
         }
     }
-    return false
+    return true
 }
 
-console.log(isPrime(15))
+console.log(isPrime(13))
