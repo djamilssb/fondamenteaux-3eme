bd29c28162d3ed3d0f27a7bf0549b842b4876103
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-10-17 10:24:19 +0200

    pyramid int

---
+++ int_pyramid.ts
@@ -0,0 +1,12 @@
+function int_pyramid(n:number):string{
+    let res:string=""
+    let textt:string=""
+    for (let i=1;i<=n;i++){
+        res = res + i.toString()
+        textt = textt + "\n" + res
+    }
+    return textt
+}
+
+let textt = int_pyramid(3)
+console.log(textt)
