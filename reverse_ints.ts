f2982dcb01fcaa6133903418343a2917b7d5e4d3
djamilssb <157473835+djamilssb@users.noreply.github.com>
2024-10-17 10:36:35 +0200

    reverse

--- reverse_ints.ts
+++ reverse_ints.ts
@@ -1,12 +1,12 @@
 function reverse_ints(n:number):string{
     let res:string=""
     let text2:string=""
-    for (let i=5;i>=n;i--){
-        res = res + i.toString()
+    for (let i=1;i<=n;i++){
+        res = i.toString() + res
         text2 = res
     }
     return text2
 }
 
-let text2 = reverse_ints(1)
+let text2 = reverse_ints(5)
 console.log(text2)
